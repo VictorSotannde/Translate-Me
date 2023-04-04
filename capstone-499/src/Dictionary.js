30eee7fced15ba143f25f51b2392d1b248a831f7
BoggieGit <99443063+BoggieGit@users.noreply.github.com>
2023-04-04 19:35:43 -0400

    Working react

--- capstone-499/src/Dictionary.js
+++ capstone-499/src/Dictionary.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
 class Dictionary extends Component {
   constructor(props) {
@@ -9,79 +10,231 @@ class Dictionary extends Component {
       editPanelVisible: false,
       languageModalVisible: false,
       textContent: '',
-      // ... Add more state variables as needed
+      searchValue: '',
+      editValue: '',
+      editDefinition: '',
+      wordList: [],
+      filteredList: [],
+      selectedLanguage: 'es',
+      translatedText: '',
     };
-
-    // Bind your event handlers
-    this.showTextModal = this.showTextModal.bind(this);
-    this.hideTextModal = this.hideTextModal.bind(this);
-    this.showEditPanel = this.showEditPanel.bind(this);
-    this.hideEditPanel = this.hideEditPanel.bind(this);
-    this.showLanguageModal = this.showLanguageModal.bind(this);
-    this.hideLanguageModal = this.hideLanguageModal.bind(this);
-    // ... Add more bindings as needed
   }
 
-  // Refactor your functions as component methods
-  showTextModal() {
+  componentDidMount() {
+    this.loadTextFile();
+  }
+
+  showTextModal = () => {
     this.setState({ textModalVisible: true });
-  }
+  };
 
-  hideTextModal() {
+  hideTextModal = () => {
     this.setState({ textModalVisible: false });
-  }
+  };
 
-  showEditPanel() {
+  showEditPanel = () => {
     this.setState({ editPanelVisible: true });
-  }
+  };
 
-  hideEditPanel() {
+  hideEditPanel = () => {
     this.setState({ editPanelVisible: false });
-  }
+  };
 
-  showLanguageModal() {
+  showLanguageModal = () => {
     this.setState({ languageModalVisible: true });
-  }
+  };
 
-  hideLanguageModal() {
+  hideLanguageModal = () => {
     this.setState({ languageModalVisible: false });
+  };
+
+  loadTextFile = async () => {
+    try {
+      const response = await fetch('/English.txt');
+      const text = await response.text();
+      this.setState({ textContent: text });
+    } catch (error) {
+      console.error('Error loading text file:', error);
+    }
+  };
+
+
+  handleSearchChange = (e) => {
+    this.setState({ searchValue: e.target.value }, () => {
+      this.handleSearch();
+    });
   }
 
-  // ... Add more methods as needed
+  handleSearch() {
+    const { searchValue, wordList } = this.state;
+    const filteredList = wordList.filter((word) =>
+      word.text.toLowerCase().includes(searchValue.toLowerCase())
+    );
+    this.setState({ filteredList });
+  }
+
+  handleAddWord() {
+    const { wordList, searchValue } = this.state;
+    const newWord = {
+      text: searchValue,
+      definition: 'New word definition',
+    };
+    this.setState(
+      {
+        wordList: [...wordList, newWord],
+        searchValue: '',
+      },
+      () => {
+        this.handleSearch();
+      }
+    );
+  }
+
+  handleDeleteWord(index) {
+    const { wordList } = this.state;
+    const updatedWordList = wordList.filter((_, idx) => idx !== index);
+    this.setState({ wordList: updatedWordList }, () => {
+      this.handleSearch();
+    });
+  }
+
+  handleEditChange(e, type) {
+    this.setState({ [type]: e.target.value });
+  }
+
+  handleEditWord(index) {
+    const { wordList, editValue, editDefinition } = this.state;
+    const updatedWordList = wordList.map((word, idx) => {
+      if (idx === index) {
+        return { text: editValue, definition: editDefinition };
+      }
+      return word;
+    });
+    this.setState({ wordList: updatedWordList }, () => {
+      this.handleSearch();
+    });
+  }
+
+  async handleTranslate() {
+    const apiKey = 'AIzaSyBioEiNvHvyhP2Ay98x_xAAs5pQGYs23P4'; // Replace with your actual API key
+    const text = this.state.textContent; // Replace with the text you want to translate
+    const targetLanguage = this.state.selectedLanguage; // Make sure you have the selected language in the state
+
+    try {
+      const response = await axios.post(`https://translation.googleapis.com/language/translate/v2?key=${apiKey}`, {
+        q: text,
+        target: targetLanguage,
+        format: 'text',
+      });
+
+      if (response.data && response.data.data && response.data.data.translations) {
+        const translatedText = response.data.data.translations[0].translatedText;
+        this.setState({ translatedText });
+      } else {
+        alert('Error occurred while translating the text');
+      }
+    } catch (error) {
+      console.error('Translation API error:', error);
+      alert('Error occurred while translating the text');
+    }
+  }
+
+  handleLanguageChange(e) {
+    this.setState({ selectedLanguage: e.target.value });
+  }
 
   render() {
-    const { textModalVisible, editPanelVisible, languageModalVisible } = this.state;
+    const {
+      textModalVisible,
+      editPanelVisible,
+      languageModalVisible,
+      filteredList,
+      editValue,
+      editDefinition,
+      selectedLanguage,
+    } = this.state;
 
     return (
       <div>
         <h1>My Webpage</h1>
         <p>Click the button below to show the text content.</p>
-        <button id="show-text-btn" onClick={this.showTextModal}>Show Text</button>
+        <button id="show-text-btn" onClick={this.showTextModal}>
+          Show Text
+        </button>
 
         {textModalVisible && (
           <div id="text-modal">
             <div className="modal-content">
-              <span className="close" onClick={this.hideTextModal}>&times;</span>
+              <span className="close" onClick={this.hideTextModal}>
+                &times;
+              </span>
               <div id="search-bar">
-                <input type="text" id="search-field" placeholder="Search..." />
+                <input
+                  type="text"
+                  id="search-field"
+                  placeholder="Search..."
+                  onChange={this.handleSearchChange}
+                />
                 <button id="search-btn">Search</button>
-                <button id="add-word-btn">+</button>
+                <button id="add-word-btn" onClick={this.handleAddWord}>+</button>
                 <button id="delete-word-btn">-</button>
               </div>
               <div id="text-container">
-                {/* Add your text content here */}
+                <pre>{this.state.textContent.split('\n').map((line, index) => (
+                  <React.Fragment key={index}>
+                    {line}
+                    <br />
+                  </React.Fragment>
+                ))}</pre>
+
               </div>
-              <button id="edit-btn" onClick={this.showEditPanel}>Edit</button>
-              <button id="change-lang-btn" onClick={this.showLanguageModal}>Change Language</button>
+              <div id="text-container">
+                {filteredList.map((word, index) => (
+                  <div key={index} className="word-entry">
+                    <h3>{word.text}</h3>
+                    <p>{word.definition}</p>
+                    <button onClick={() => this.handleDeleteWord(index)}>
+                      Delete
+                    </button>
+                    <button onClick={() => this.showEditPanel(index)}>
+                      Edit
+                    </button>
+                  </div>
+                ))}
+              </div>
+              <button id="edit-btn" onClick={this.showEditPanel}>
+                Edit
+              </button>
+              <button id="change-lang-btn" onClick={this.showLanguageModal}>
+                Change Language
+              </button>
 
               {editPanelVisible && (
                 <div id="edit-panel">
                   <div id="edit-container">
-                    <input type="text" id="edit-word" placeholder="Word..." />
-                    <textarea id="edit-definition" placeholder="Definition..." rows="5"></textarea>
+                    <input
+                      type="text"
+                      id="edit-word"
+                      placeholder="Word..."
+                      value={editValue}
+                      onChange={(e) => this.handleEditChange(e, 'editValue')}
+                    />
+                    <textarea
+                      id="edit-definition"
+                      placeholder="Definition..."
+                      rows="5"
+                      value={editDefinition}
+                      onChange={(e) =>
+                        this.handleEditChange(e, 'editDefinition')
+                      }
+                    ></textarea>
                   </div>
-                  <button id="save-btn" onClick={this.hideEditPanel}>Save</button>
-                  <button id="cancel-btn" onClick={this.hideEditPanel}>Cancel</button>
+                  <button id="save-btn" onClick={this.handleEditWord}>
+                    Save
+                  </button>
+                  <button id="cancel-btn" onClick={this.hideEditPanel}>
+                    Cancel
+                  </button>
                 </div>
               )}
 
@@ -90,14 +243,13 @@ class Dictionary extends Component {
                   <div className="modal-content">
                     <span className="close-lang" onClick={this.hideLanguageModal}>&times;</span>
                     <h2>Select Language</h2>
-                    <select id="language-select">
+                    <select id="language-select" onChange={this.handleLanguageChange}>
                       <option value="es">Spanish</option>
-                     
                       <option value="fr">French</option>
                       <option value="de">German</option>
                       {/* Add more languages here */}
                     </select>
-                    <button id="translate-btn">Translate</button>
+                    <button id="translate-btn" onClick={this.handleTranslate}>Translate</button>
                     <button id="cancel-translate-btn" onClick={this.hideLanguageModal}>Cancel</button>
                     <button id="translate-back-btn">Translate Back to English</button>
                   </div>
@@ -110,5 +262,4 @@ class Dictionary extends Component {
     );
   }
 }
-
 export default Dictionary;
